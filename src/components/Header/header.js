1fd04c4064038a6f2edae6c9ddec6f9cb66e4b30
Katerina <katerinalupacheva@gmail.com>
2020-01-05 16:19:27 +0300

    Logo added

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import { ThemeToggler } from 'gatsby-plugin-dark-mode';
 import moon from '../../../static/media/moon.png';
 import sun from '../../../static/media/sun.png';
+import logo_dark from '../../../static/media/logo_dark.png';
+import logo_light from '../../../static/media/logo_light.png';
 import styles from './header.module.scss';
 
 const Header = () => {
     const [icon, toggleIcon ] = useState(localStorage.theme === 'dark' ? sun : moon);
+    const [logo, toggleLogo ] = useState(localStorage.theme === 'dark' ? logo_light : logo_dark);
 
     return (
         <div className={styles['header-container']}>
+            <img src={logo} alt='Logo' className={styles['logo']} width={70}/>
+            <div className={styles['border']}></div>
             <ThemeToggler>
                 {({ theme, toggleTheme }) => (                  
                 <label>
@@ -18,10 +23,11 @@ const Header = () => {
                         onChange={e => {
                             toggleTheme(e.target.checked ? 'dark' : 'light');
                             toggleIcon(e.target.checked ? sun : moon);
+                            toggleLogo(e.target.checked ? logo_light : logo_dark);
                         }}
                         checked={theme === 'dark'}
-                    />
-                    <img src={icon} width={30} height={30} />
+                    /> 
+                    <img src={icon} width={30} height={30} className={styles['theme-toggler']} />
                 </label>
                 )}
             </ThemeToggler>
