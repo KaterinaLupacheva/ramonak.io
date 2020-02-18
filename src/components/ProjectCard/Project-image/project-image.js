ccae392c6a48b4979b7f0dbc8d7d9425c2ff1cf8
Katerina <katerinalupacheva@gmail.com>
2020-02-18 17:28:29 +0300

    Project image animated

--- src/components/ProjectCard/Project-image/project-image.js
+++ src/components/ProjectCard/Project-image/project-image.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useSpring, animated} from 'react-spring';
 import styles from './project-image.module.scss';
 
-const ProjectImage = ({link, image}) => (
-    <div className={styles['image-container']}>
+const ProjectImage = ({link, image}) => {
+    const [isHovered, setHovered] = useState(false);
+    const props = useSpring({ transform: `scale(${isHovered ? 1.1 : 1})`});
+
+    return(
+    <animated.div className={styles['image-container']}
+        onMouseOver={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        style={props}
+    >
         <a href={link} target='_blank'>
             <img className={styles['project-image']}
                 src={image}
             />
         </a>
-    </div>
-);
+    </animated.div>
+)};
 
 export default ProjectImage;
