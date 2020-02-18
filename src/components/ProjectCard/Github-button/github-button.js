204cef9441bda4a5aa7e2d4990a040d241284baa
Katerina <katerinalupacheva@gmail.com>
2020-02-18 16:57:39 +0300

    Spring animation added to github-button component

--- src/components/ProjectCard/Github-button/github-button.js
+++ src/components/ProjectCard/Github-button/github-button.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useSpring, animated} from 'react-spring';
 import Icon from '../../Icon/Icon';
 import getIcon from '../../../utils/get-icon';  
 import styles from './github-button.module.scss';
 
-const GithubButton = ({ href }) => (
-    <div className={styles['github-button']} >
+const GithubButton = ({ href }) => {
+    const [isHovered, setHovered] = useState(false);
+    const props = useSpring({ transform: `scale(${isHovered ? 1.2 : 1})`});
+    
+    return(
+    <animated.div className={styles['github-button']} 
+        onMouseOver={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        style={props}
+    >
         <a href={href} target='_blank'>
             <Icon name={'source'} icon={getIcon('github')} />
             <span>Source</span>
         </a>
-    </div>
-);
+    </animated.div>
+)};
 
 export default GithubButton;
