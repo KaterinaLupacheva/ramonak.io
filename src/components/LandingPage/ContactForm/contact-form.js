ba2503f0378ec8db38ca34323f8d17c35cd354cd
Katerina <katerinalupacheva@gmail.com>
2020-05-14 18:40:43 +0300

    Contact form styling

--- src/components/LandingPage/ContactForm/contact-form.js
+++ src/components/LandingPage/ContactForm/contact-form.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "./contact-form.module.scss";
 
 const ContactForm = () => {
   const INITIAL_STATE = {
@@ -14,16 +15,51 @@ const ContactForm = () => {
       [e.target.name]: e.target.value,
     });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("SUBMIT");
+  };
   return (
-    <div>
-      <form>
-        <label htmlFor="name">Your Name</label>
+    <div className={styles["container"]}>
+      <h1>Get in touch</h1>
+      <form onSubmit={handleSubmit}>
+        <div className={styles["form-fields"]}>
+          <div className={styles["column"]}>
+            <label htmlFor="name">Your name*</label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              required
+              value={state.name}
+              onChange={handleChange}
+            />
+            <label htmlFor="email">Your email*</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              required
+              value={state.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles["column"]}>
+            <label htmlFor="message">Your message*</label>
+            <textarea
+              id="message"
+              name="message"
+              required
+              value={state.message}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
         <input
-          id="name"
-          type="text"
-          name="name"
-          value={state.name}
-          onChange={handleChange}
+          type="submit"
+          value="Submit"
+          className={styles["submit-button"]}
         />
       </form>
     </div>
