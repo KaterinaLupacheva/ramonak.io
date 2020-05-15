be3a1cb1b0b9a21519072cd450f2bbceb0ed2011
Katerina <katerinalupacheva@gmail.com>
2020-05-15 18:36:02 +0300

    White bg updated and description section

--- src/components/LandingPage/Description/description.js
+++ src/components/LandingPage/Description/description.js
@@ -1,13 +1,16 @@
 import React from "react";
-import styles from './description.module.scss';
+import styles from "./description.module.scss";
 
 const Description = () => {
   return (
-    <div className={styles['container']}>
-      <img src="/media/projects/coffee-dashboard.JPG" />
-      <div className={styles['description']}>
-        text text text text text text text text text text text text text text
-        text text text text text text text text text text
+    <div className={styles["container"]}>
+      <h1>My Recent Work</h1>
+      <div className={styles["section"]}>
+        <img src="/media/projects/coffee-dashboard.JPG" />
+        <div className={styles["description"]}>
+          text text text text text text text text text text text text text text
+          text text text text text text text text text text
+        </div>
       </div>
     </div>
   );
