9f394e2b389bb949564ae6a404bf3aa03d323189
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-10-12 15:25:45 +0300

    Heroku digital garden added

--- src/components/DigitalGarden/Heroku.js
+++ src/components/DigitalGarden/Heroku.js
@@ -1,13 +1,27 @@
 import React from "react";
+import styles from "./shared.module.scss";
 
 const Heroku = () => (
-  <div>
-    <h3>Export database from Heroku</h3>
-    <div>1. Create backup</div>
-    <code>{`heroku pg:backups:capture --app <heroku_app_name>`}</code>
-    <div>2. Download backup</div>
-    <code>{`heroku pg:backups:download --app <heroku_app_name>`}</code>
-  </div>
+  <>
+    <div className={styles["block"]}>
+      <h3>Export database from Heroku</h3>
+      <div className={styles["action"]}>1. Create backup</div>
+      <div
+        className={styles["code"]}
+      >{`heroku pg:backups:capture --app <heroku_app_name>`}</div>
+      <div className={styles["action"]}>2. Download backup</div>
+      <div
+        className={styles["code"]}
+      >{`heroku pg:backups:download --app <heroku_app_name>`}</div>
+    </div>
+
+    <div className={styles["block"]}>
+      <h3>Restore dump to local database</h3>
+      <div
+        className={styles["code"]}
+      >{`pg_restore --verbose --clean --no-acl --no-owner -h localhost -U <postgres_username> -d <database> latest.dump`}</div>
+    </div>
+  </>
 );
 
 export default Heroku;
