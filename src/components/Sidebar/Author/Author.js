401a9ba05c6813bb43d877748cc391ddcdf8c00d
Katsiaryna Lupachova <katerinalupacheva@gmail.com>
2023-01-09 17:20:45 +0300

    Cleanup

--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -1,9 +1,8 @@
 // @flow strict
-import React, { useState } from "react";
-import { withPrefix, Link } from "gatsby";
-import map from "../../../../static/media/map.png";
-import styles from "./Author.module.scss";
-import * as flag from "./flag.png";
+import React, { useState } from 'react';
+import { withPrefix, Link } from 'gatsby';
+import map from '../../../../static/media/map.png';
+import styles from './Author.module.scss';
 
 type Props = {
   author: {
@@ -16,46 +15,45 @@ type Props = {
 
 const Author = ({ author, isIndex }: Props) => {
   const [isVisible, toggleIsVisible] = useState(false);
-  const container = "mapContainer";
+  const container = 'mapContainer';
   return (
-    <div className={styles["author"]}>
-      <div className={styles["author__container"]}>
+    <div className={styles['author']}>
+      <div className={styles['author__container']}>
         <Link to="/">
           <img
             src={withPrefix(author.photo)}
-            className={styles["author__photo"]}
+            className={styles['author__photo']}
             width="75"
             height="75"
             alt={author.name}
           />
         </Link>
-        <img src={flag} width="75" height="75" alt="flag" />
       </div>
       {isIndex === true ? (
-        <h1 className={styles["author__title"]}>
-          <Link className={styles["author__title-link"]} to="/">
+        <h1 className={styles['author__title']}>
+          <Link className={styles['author__title-link']} to="/">
             {author.name}
           </Link>
         </h1>
       ) : (
-        <h2 className={styles["author__title"]}>
-          <Link className={styles["author__title-link"]} to="/">
+        <h2 className={styles['author__title']}>
+          <Link className={styles['author__title-link']} to="/">
             {author.name}
           </Link>
         </h2>
       )}
 
-      <p className={styles["author__subtitle"]}>
-        Mobile and web apps developer, freelancer based in{" "}
+      <p className={styles['author__subtitle']}>
+        Mobile and web apps developer, freelancer based in{' '}
         <span
-          className={styles["belarus"]}
+          className={styles['belarus']}
           onClick={() => toggleIsVisible(!isVisible)}
         >
           Belarus
-        </span>{" "}
+        </span>{' '}
         (Europe).
       </p>
-      <div className={styles[`${isVisible ? "visible" : ""}mapContainer`]}>
+      <div className={styles[`${isVisible ? 'visible' : ''}mapContainer`]}>
         <img className={styles.map} src={map} alt="Europe Map" />
       </div>
     </div>
